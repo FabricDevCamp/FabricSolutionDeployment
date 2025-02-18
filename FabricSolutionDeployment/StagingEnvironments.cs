640ab2783934ce449474c87e0eaad4acf8a505ab
Ted Pattison <ted@tedpattison.net>
2025-02-18 00:15:09 -0500

    Update StagingEnvironments.cs

--- FabricSolutionDeployment/StagingEnvironments.cs
+++ FabricSolutionDeployment/StagingEnvironments.cs
@@ -2,33 +2,26 @@
 
   public static DeploymentPlan Dev {
     get {
-
       var Deployment = new DeploymentPlan();
       Deployment.CustomerName = "Dev";
 
       // setup Web datasource path
-      string deploymentWebDatasourcePath = DeploymentPlan.webDatasourceRootDefault + "Dev/";
-
-      Deployment.AddDeploymentParameter(
-                          DeploymentPlan.webDatasourcePathParameter,
-                          DeploymentPlan.webDatasourcePathDefault,
-                          deploymentWebDatasourcePath);
+      Deployment.AddDeploymentParameter(DeploymentPlan.webDatasourcePathParameter,
+                                        DeploymentPlan.webDatasourcePathDefault,
+                                        DeploymentPlan.webDatasourceRootDefault + "Dev/");
 
       // setup ADLS datasource path
-      Deployment.AddDeploymentParameter(
-                    DeploymentPlan.adlsServerPathParameter,
-                    DeploymentPlan.adlsServerPathDefault,
-                    DeploymentPlan.adlsServerPathDefault);
+      Deployment.AddDeploymentParameter(DeploymentPlan.adlsServerPathParameter,
+                                        DeploymentPlan.adlsServerPathDefault,
+                                        DeploymentPlan.adlsServerPathDefault);
 
-      Deployment.AddDeploymentParameter(
-                   DeploymentPlan.adlsContainerNameParameter,
-                   DeploymentPlan.adlsContainerNameDefault,
-                   DeploymentPlan.adlsContainerNameDefault);
+      Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerNameParameter,
+                                        DeploymentPlan.adlsContainerNameDefault,
+                                        DeploymentPlan.adlsContainerNameDefault);
 
-      Deployment.AddDeploymentParameter(
-                   DeploymentPlan.adlsContainerPathParameter,
-                   DeploymentPlan.adlsContainerPathDefault,
-                    "/ProductSales/Dev/");
+      Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerPathParameter,
+                                        DeploymentPlan.adlsContainerPathDefault,
+                                        "/ProductSales/Dev/");
 
       return Deployment;
     }
@@ -36,26 +29,26 @@
 
   public static DeploymentPlan Test {
     get {
-
       var Deployment = new DeploymentPlan();
       Deployment.CustomerName = "Test";
 
+      // setup Web datasource path
       Deployment.AddDeploymentParameter(DeploymentPlan.webDatasourcePathParameter,
-                                      DeploymentPlan.webDatasourcePathDefault,
-                                      DeploymentPlan.webDatasourceRootDefault + "Test/");
+                                        DeploymentPlan.webDatasourcePathDefault,
+                                        DeploymentPlan.webDatasourceRootDefault + "Test/");
 
       // setup ADLS datasource path
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsServerPathParameter,
-                                      DeploymentPlan.adlsServerPathDefault,
-                                      DeploymentPlan.adlsServerPathDefault);
+                                        DeploymentPlan.adlsServerPathDefault,
+                                        DeploymentPlan.adlsServerPathDefault);
 
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerNameParameter,
-                                      DeploymentPlan.adlsContainerNameDefault,
-                                      DeploymentPlan.adlsContainerNameDefault);
+                                        DeploymentPlan.adlsContainerNameDefault,
+                                        DeploymentPlan.adlsContainerNameDefault);
 
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerPathParameter,
-                                      DeploymentPlan.adlsContainerPathDefault,
-                                      "/ProductSales/Test");
+                                        DeploymentPlan.adlsContainerPathDefault,
+                                        "/ProductSales/Test");
 
       return Deployment;
     }
@@ -63,26 +56,26 @@
 
   public static DeploymentPlan Prod {
     get {
-
       var Deployment = new DeploymentPlan();
       Deployment.CustomerName = "Prod";
 
+      // setup Web datasource path
       Deployment.AddDeploymentParameter(DeploymentPlan.webDatasourcePathParameter,
-                                      DeploymentPlan.webDatasourcePathDefault,
-                                      DeploymentPlan.webDatasourceRootDefault + "Prod/");
+                                        DeploymentPlan.webDatasourcePathDefault,
+                                        DeploymentPlan.webDatasourceRootDefault + "Prod/");
 
       // setup ADLS datasource path
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsServerPathParameter,
-                                      DeploymentPlan.adlsServerPathDefault,
-                                      DeploymentPlan.adlsServerPathDefault);
+                                        DeploymentPlan.adlsServerPathDefault,
+                                        DeploymentPlan.adlsServerPathDefault);
 
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerNameParameter,
-                                      DeploymentPlan.adlsContainerNameDefault,
-                                      DeploymentPlan.adlsContainerNameDefault);
+                                        DeploymentPlan.adlsContainerNameDefault,
+                                        DeploymentPlan.adlsContainerNameDefault);
 
       Deployment.AddDeploymentParameter(DeploymentPlan.adlsContainerPathParameter,
-                                      DeploymentPlan.adlsContainerPathDefault,
-                                      "/ProductSales/Prod");
+                                        DeploymentPlan.adlsContainerPathDefault,
+                                        "/ProductSales/Prod");
 
       return Deployment;
     }
